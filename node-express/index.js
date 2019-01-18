1aa8c873a9a60f5d2a5db22f1ababc3629784a5d
Sudhir Kumar <sudhir14107@iiitd.ac.in>
2019-01-18 23:37:13 +0530

    First assignment completed using express,rest

--- node-express/index.js
+++ node-express/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const hostname = "localhost";
 const port = 3000;
 
 const app= express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next) => {
+    res.statusCode =200;
+    res.setHeader('Content-Type','text/plain');
+    next();//look for additional information
+});
+
+app.get('/dishes', (req,res,next) => {
+    res.end('will send all dishes to you');
+});
+
+app.post('/dishes',(req,res,next) => {
+    res.end(`will add the dish : ${req.body.name}`)
+});
 
 app.use(express.static(__dirname+'/public'))
 app.use((req, res, next) => {

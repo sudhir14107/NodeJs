a21cc2eb70a6a3f222ce6d92bdc413ad87b95440
Sudhir Kumar <sudhir14107@iiitd.ac.in>
2019-01-18 17:10:34 +0530

    Express Serve static file

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const http = require('http');
-
-const hostname = "localhost"
+const morgan = require('morgan');
+const hostname = "localhost";
 const port = 3000;
 
 const app= express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public'))
 app.use((req, res, next) => {
-    console.log(req.header);
+    
     res.statusCode = 404;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>This is an Express server</h1></body></html>');
